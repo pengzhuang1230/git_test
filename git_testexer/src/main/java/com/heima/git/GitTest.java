4a5312dcbf2d34e301dc52c6cea53c5833753bff
pzand <123@123.com>
2018-10-14 19:10:03 +0800

    第二次提交：修改GitTest.class

--- git_testexer/src/main/java/com/heima/git/GitTest.java
+++ git_testexer/src/main/java/com/heima/git/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("upload javaProject to gitHub");
     }
+
+    public static void method1(){
+        System.out.println("update and push project to git");
+    }
 }
