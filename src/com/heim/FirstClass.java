51d5ce49f03d4f5f2ffcca1d0f2f1463f7e22b93
pzand <123@123.com>
2018-10-14 17:13:22 +0800

    第一次提交

--- src/com/heim/FirstClass.java
+++ src/com/heim/FirstClass.java
@@ -4,4 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public static void  method(){
+
+        System.out.println("hello update");
+    }
 }
